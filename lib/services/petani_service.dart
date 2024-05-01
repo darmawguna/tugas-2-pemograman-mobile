2fa10206c9df234318bb7b3c9cadd8b9653ff15f
darmawguna <komangagus642@gmail.com>
2024-05-01 21:29:33 +0800

    update

--- lib/services/petani_service.dart
+++ lib/services/petani_service.dart
@@ -2,9 +2,26 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tugaske2/models/kelompok_tani_model.dart';
+import 'package:tugaske2/models/new_petani_model.dart';
 import 'package:tugaske2/models/petani_model.dart';
 
+
 class APiService {
+  //static final host='http://192.168.43.189/webtani/public';
+  static final host = 'http://10.10.58.123/webtani/public';
+  static var _token = "8|x6bKsHp9STb0uLJsM11GkWhZEYRWPbv0IqlXvFi7";
+  Future<SharedPreferences> preferences = SharedPreferences.getInstance();
+  static Future<void> getPref() async {
+    Future<SharedPreferences> preferences = SharedPreferences.getInstance();
+    final SharedPreferences prefs = await preferences;
+    _token = prefs.getString('token') ?? "";
+  }
+
+  static getHost() {
+    return host;
+  }
   // Fetch all petani
   Future<List<Petani>> fetchPetani() async {
     final response =
@@ -84,6 +101,25 @@ class APiService {
 
     if (response.statusCode != 200) {
       throw Exception('Failed to delete petani');
+    }
+  }
+
+  // get kelompok petani
+  static Future<List<Kelompok>> getKelompokTani() async {
+    try {
+      final response =
+          await http.get(Uri.parse("$host/api/kelompoktani"), headers: {
+        'Authorization': 'Bearer ' + _token,
+      });
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        final parsed = json.cast<Map<String, dynamic>>();
+        return parsed.map<Kelompok>((json) => Kelompok.fromJson(json)).toList();
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
     }
   }
 }
